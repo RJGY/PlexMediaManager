ba5a1e1901863d9fe08fab6e3d2f23b0c0c283ca
RJGY <rjgy2yu@gmail.com>
2022-04-28 23:17:07 +1000

    started bot code

--- bot/bot.py
+++ bot/bot.py
@@ -1,1 +1,18 @@
-import discord.py
+import discord
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run('')
