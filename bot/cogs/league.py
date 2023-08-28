4fd03ada2056ed9c2cdba52992e6d37804e40c83
Reese Gunardi <rjgy2yu@gmail.com>
2023-08-29 00:58:04 +1000

    made it on a 1 hour timer now and allow a manual update every 2 mins

--- bot/cogs/league.py
+++ bot/cogs/league.py
@@ -112,9 +112,9 @@ class League(commands.Cog):
         self.api = LeagueAPI(AUTH_TOKEN)
         self.api.get_players_ids()
         
-    @commands.command(name = "refresh", help = "Refreshes the list of players in the league channel")
+    @commands.command(name = "ladder", help = "Manually retrieves the ranked ladder data.")
     @commands.cooldown(1, 120, commands.BucketType.guild)
-    async def refresh_command(self, ctx):
+    async def manual_leaderboard_command(self, ctx):
         await ctx.send("Refreshing...")
         self.api.flush_players()
         self.api.get_players_rank()
@@ -134,6 +134,11 @@ class League(commands.Cog):
             embed.add_field(name=f'{i+1}. {self.api.players[i].name}', value=f'{self.api.players[i].tier} {self.api.players[i].rank} {self.api.players[i].lp} LP\n {self.api.players[i].wins}W {self.api.players[i].losses}L Winrate {self.api.players[i].winrate}%', inline=False)
         await ctx.send(embed=embed)
         
+    @manual_leaderboard_command.error
+    async def manual_leaderboard_command_error(self, ctx, exc):
+        if isinstance(exc, discord.ext.commands.errors.CommandOnCooldown):
+            await ctx.send("Command on cooldown. Please wait " + str(exc.retry_after) + " seconds.")
+        
     @commands.command(name = "leaderboard", help = "Shows the current leaderboard")
     async def toggle_leaderboard_command(self, ctx):
         
@@ -142,7 +147,7 @@ class League(commands.Cog):
             await ctx.send("You are not him. You cannot use this command.")
             return
         else:
-            him_message = await ctx.send("You are him. You can use this command.")
+            await ctx.send("You are him. You can use this command.")
         """completely unneccessary just here for fun lmao"""
         
         if self.leaderboard and self.toggle_leaderboard:
@@ -151,7 +156,6 @@ class League(commands.Cog):
             self.leaderboard_channel = None
             await ctx.send("Leaderboard update disabled.")
             self.auto_leaderboard.cancel()
-            await him_message.delete()
             return
             
         await ctx.send("Loading...")
@@ -176,9 +180,8 @@ class League(commands.Cog):
         self.toggle_leaderboard = True
         self.leaderboard_member = ctx.author
         self.auto_leaderboard.start()
-        await him_message.delete()
         
-    @tasks.loop(seconds=120)
+    @tasks.loop(hours=1)
     async def auto_leaderboard(self):
         if self.toggle_leaderboard:
             refresh_message = await self.leaderboard_channel.send("Refreshing...")
@@ -201,13 +204,38 @@ class League(commands.Cog):
             await self.leaderboard.edit(embed=embed)
             await refresh_message.delete()
     
-    @refresh_command.error
-    async def refresh_command_error(self, ctx, exc):
+    @commands.command(name = "refresh", help = "Refreshes the automatic leaderboard.")
+    @commands.cooldown(1, 120, commands.BucketType.guild)
+    async def manual_refresh(self, ctx):
+        if self.toggle_leaderboard:
+            refresh_message = await self.leaderboard_channel.send("Refreshing...")
+            self.api.flush_players()
+            self.api.get_players_rank()
+            self.api.convert_players_ranks()
+            self.api.sort_players()
+            self.api.convert_back_players_ranks()
+            embed = discord.Embed(
+                title="Leaderboard",
+                description=f"Showing how boosted kaia is :3", 
+                colour=discord.Colour.green(),
+                timestamp=dt.datetime.now()
+            )
+            
+            embed.set_author(name="League of Legends Rank Leaderboard")
+            embed.set_footer(text=f"Requested by {self.leaderboard_member.display_name}", icon_url=self.leaderboard_member.display_avatar.url)
+            for i in range(len(self.api.players)):
+                embed.add_field(name=f'{i+1}. {self.api.players[i].name}', value=f'{self.api.players[i].tier} {self.api.players[i].rank} {self.api.players[i].lp} LP\n {self.api.players[i].wins}W {self.api.players[i].losses}L Winrate {self.api.players[i].winrate}%', inline=False)
+            await self.leaderboard.edit(embed=embed)
+            await refresh_message.delete()
+            await ctx.message.delete()
+            
+        
+    @manual_refresh.error
+    async def manual_refresh_error(self, ctx, exc):
         if isinstance(exc, discord.ext.commands.errors.CommandOnCooldown):
             await ctx.send("Command on cooldown. Please wait " + str(exc.retry_after) + " seconds.")
-        
-        
-
+            
+            
 async def setup(bot):
     await bot.add_cog(League(bot))
     
